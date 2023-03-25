41118d1ca974b22ce4320633924b4de4de2b30a4
Samz685 <94040038+Samz685@users.noreply.github.com>
2023-03-25 20:16:25 +0100

    Añadido objeto SOLICITUD

--- ReservarApp/app/src/main/java/com/example/reservarapp/models/Reserva.kt
+++ ReservarApp/app/src/main/java/com/example/reservarapp/models/Reserva.kt
@@ -7,8 +7,8 @@ class Reserva {
 
     var id: String = ""
     var grupo: String? = ""
-    var alias: String? = ""
     var cliente: String? = ""
+    var telefono: String? = ""
     var fecha: Date = Date()
     var numComensales: Int = 0
     var disposicion: String? = ""
@@ -17,7 +17,7 @@ class Reserva {
 
 
     override fun toString(): String {
-        return "Reserva(id='$id', grupo=$grupo, alias=$alias, cliente=$cliente, fecha=$fecha, numComensales=$numComensales, disposicion=$disposicion, comentario=$comentario, owner=$owner)"
+        return "Reserva(id='$id', grupo=$grupo, telefono=$telefono, cliente=$cliente, fecha=$fecha, numComensales=$numComensales, disposicion=$disposicion, comentario=$comentario, owner=$owner)"
     }
 
 
---
+++ ReservarApp/app/src/main/java/com/example/reservarapp/models/Solicitud.kt
@@ -0,0 +1,12 @@
+package com.example.reservarapp.models
+
+import java.util.Date
+
+class Solicitud {
+
+    var fromUser : String = ""
+    var toGroup : String = ""
+    var date : Date = Date()
+    var estado : String = ""
+
+}
